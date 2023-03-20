958d8478a7a2bf274d11917724ac72dd0cac8cb9
Altenia Gaines <gaines.tina90@gmail.com>
2023-03-19 17:53:30 -0700

    add conditionals

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue < 400) {
+    result = "This one is on me!";
+  }
+  else if ( someValue > 400 && someValue <2000){
+    result = "That will be twenty bucks."
+  }
+  else if (someValue > 2500){
+    result = "No can do."
+  }
+  else {
+    result = "I will gladly take your thirty bucks."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === "NYC"? "Ok, sounds good.": "No go." 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you."
+    default: 
+      return "Bye."
+  }
 }
